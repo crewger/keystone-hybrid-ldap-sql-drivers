7276e1b9dc8a4e4580f355980ceafa87a030390b
Konstantin. Tarasiuk <ktarasiuk@mirantis.com>
2016-09-21 13:49:48 +0300

    added the drivers

---
+++ hybrid_assignment.py
@@ -0,0 +1,158 @@
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+# Copyright 2014 SUSE Linux Products GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo.config import cfg
+from keystone import config
+from keystone.assignment.backends import sql as sql_assign
+from keystone.assignment.role_backends import sql as sql_role
+from keystone.common import sql
+from keystone import exception
+from keystone.identity.backends import ldap as ldap_backend
+from oslo_utils import importutils
+from oslo_log import log
+
+LOG = log.getLogger(__name__)
+
+hybrid_opts = [
+    cfg.ListOpt('default_roles',
+                default=['_member_', ],
+                help='List of roles assigned by default to an LDAP user'),
+    cfg.StrOpt('default_project',
+               default='demo',
+               help='Default project'),
+    cfg.StrOpt('default_domain',
+               default='default',
+               help='Default domain'),
+]
+
+CONF = config.CONF
+CONF.register_opts(hybrid_opts, 'ldap_hybrid')
+
+
+class Assignment(sql_assign.Assignment):
+    _default_roles = list()
+    _default_project = None
+
+    def __init__(self, *args, **kwargs):
+      super(Assignment, self).__init__(*args, **kwargs)
+      self.ldap_user = ldap_backend.UserApi(CONF)
+      self.resource_driver = importutils.import_object(self.default_resource_driver())
+
+    def _get_metadata(self, user_id=None, tenant_id=None,
+                      domain_id=None, group_id=None, session=None):
+        # We only want to apply 'default_roles' to users from LDAP, so
+        # check if this is an LDAP User first
+        is_ldap = False
+        try:
+            self.ldap_user.get(user_id)
+        except exception.UserNotFound:
+            # Not an LDAP User
+            pass
+        else:
+            is_ldap = True
+
+        try:
+            res = super(Assignment, self)._get_metadata(
+                user_id, tenant_id, domain_id, group_id, session)
+        except exception.MetadataNotFound:
+            if self.default_project_id == tenant_id and is_ldap:
+                return {
+                    'roles': [
+                        {'id': role_id} for role_id in self.default_roles
+                    ]
+                }
+            else:
+                raise
+        else:
+            if is_ldap:
+              roles = res.get('roles', [])
+              res['roles'] = roles + [
+                  {'id': role_id} for role_id in self.default_roles
+              ]
+            return res
+
+    @property
+    def default_project(self):
+        if self._default_project is None:
+            self._default_project = self.resource_driver.get_project_by_name(
+                CONF.ldap_hybrid.default_project,
+                CONF.ldap_hybrid.default_domain)
+        return dict(self._default_project)
+
+    @property
+    def default_project_id(self):
+        return self.default_project['id']
+
+    @property
+    def default_roles(self):
+        if not self._default_roles:
+            with sql.transaction() as session:
+                query = session.query(sql_role.RoleTable)
+                query = query.filter(sql_role.RoleTable.name.in_(
+                    CONF.ldap_hybrid.default_roles))
+                role_refs = query.all()
+
+            if len(role_refs) != len(CONF.ldap_hybrid.default_roles):
+                raise exception.RoleNotFound(
+                    message=_('Could not find one or more roles: %s') %
+                    ', '.join(CONF.ldap_hybrid.default_roles))
+
+            self._default_roles = [role_ref.id for role_ref in role_refs]
+        return self._default_roles
+
+    def list_projects_for_user(self, user_id, group_ids, hints):
+        projects = super(Assignment, self).list_projects_for_user(
+            user_id, group_ids, hints)
+
+        # Make sure the default project is in the project list for the user
+        # user_id
+        for project in projects:
+            if project['id'] == self.default_project_id:
+                return projects
+
+        # We only want to apply 'default_project' to users from LDAP, so
+        # check if this is an LDAP User first
+        try:
+            self.ldap_user.get(user_id)
+        except exception.UserNotFound:
+            # Not an LDAP User
+            pass
+        else:
+            projects.append(self.default_project)
+
+        return projects
+
+    def list_project_ids_for_user(self, user_id, group_ids, hints):
+        project_ids = super(Assignment, self).list_project_ids_for_user(
+            user_id, group_ids, hints)
+
+        LOG.debug("project_ids: %s", project_ids)
+        # Make sure the default project is in the project list for the user
+        # user_id
+        for project_id in project_ids:
+            if project_id == self.default_project_id:
+                return project_ids
+
+        # We only want to apply 'default_project' to users from LDAP, so
+        # check if this is an LDAP User first
+        try:
+            self.ldap_user.get(user_id)
+        except exception.UserNotFound:
+            # Not an LDAP User
+            pass
+        else:
+            project_ids.append(self.default_project_id)
+
+        return project_ids
---
+++ hybrid_identity.py
@@ -0,0 +1,148 @@
+# Copyright 2012-2014 SUSE Linux Products GmbH
+# Copyright 2012 OpenStack LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+"""Hybrid Identity backend for Keystone on top of the LDAP and SQL backends"""
+
+from keystone.common import dependency
+from keystone.common import sql
+from keystone.common import utils
+from keystone import exception
+from keystone import identity
+from keystone.identity.backends import ldap as ldap_backend
+from keystone.identity.backends import sql as sql_ident
+
+from oslo_config import cfg
+from oslo_log import log
+
+CONF = cfg.CONF
+LOG = log.getLogger(__name__)
+
+
+@dependency.requires('assignment_api')
+class Identity(sql_ident.Identity):
+    def __init__(self, *args, **kwargs):
+        super(Identity, self).__init__(*args, **kwargs)
+        self.ldap = ldap_backend.Identity(CONF)
+        self.domain_aware = True
+
+    # Identity interface
+    def authenticate(self, user_id, password):
+        """Authenticate based on a user and password.
+        Tries to authenticate using the SQL backend first, if that fails
+        it tries the LDAP backend.
+        """
+        if not password:
+            raise AssertionError('Invalid user / password')
+
+        session = sql.get_session()
+        try:
+            user_ref = self._get_user(session, user_id)
+        except exception.UserNotFound:
+            raise AssertionError('Invalid user / password')
+
+        try:
+            # if the user_ref has a password, it's from the SQL backend and
+            # we can just check if it coincides with the one we got
+            assert utils.check_password(password, user_ref['password'])
+        except TypeError:
+            raise AssertionError('Invalid user / password')
+        except KeyError:  # if it doesn't have a password, it must be LDAP
+            conn = None
+            try:
+                # get_connection does a bind for us which checks the password
+                #conn = self.user.get_connection(self.user._id_to_dn(user_id),
+                #                                password)
+                dn = self.ldap.user._id_to_dn(user_id)
+                conn = self.ldap.user.get_connection(dn, password)
+                assert conn
+            except Exception:
+                raise AssertionError('Invalid user / password')
+            else:
+                LOG.debug("Authenticated user with LDAP.")
+                self.domain_aware = False
+            finally:
+                if conn:
+                    conn.unbind_s()
+        else:
+            LOG.debug("Authenticated user with SQL.")
+            # turn the SQLAlchemy User object into a dict to match what
+            # LDAP would return
+            user_ref = user_ref.to_dict()
+
+        return identity.filter_user(user_ref)
+
+    def is_domain_aware(self):
+        # XXX we only need domain_aware to be False when authenticating
+        # as an LDAP user; after that, all operations will be done on
+        # the SQL database and domain_aware needs to be True. This code
+        # makes the assumption that the result of authenticate() should
+        # be read as not domain_aware (for LDAP), after which
+        # domain_aware should revert to True.
+        domain_aware = self.domain_aware
+        if not self.domain_aware:
+            self.domain_aware = True
+        return domain_aware
+
+    def _get_user(self, session, user_id):
+        # try SQL first
+        try:
+            user_ref = super(Identity, self)._get_user(session, user_id)
+        except exception.UserNotFound:
+            # then try LDAP
+            user_ref = self.ldap.user.get(user_id)
+            user_ref['domain_id'] = CONF.identity.default_domain_id
+            return user_ref
+        else:
+            return user_ref
+
+    def get_user(self, user_id):
+        LOG.debug("Called get_user %s" % user_id)
+        session = sql.get_session()
+        user = self._get_user(session, user_id)
+        try:
+            user = user.to_dict()
+        except AttributeError:
+            # LDAP Users are already dicts which is fine
+            pass
+        return identity.filter_user(user)
+
+    def get_user_by_name(self, user_name, domain_id):
+        LOG.debug("Called get_user_by_name %s, %s" % (user_name, domain_id))
+        # try SQL first
+        try:
+            user = super(Identity, self).get_user_by_name(user_name, domain_id)
+        except exception.UserNotFound:
+            # then try LDAP
+            user = identity.filter_user(self.ldap.user.get_by_name(user_name))
+            user['domain_id'] = CONF.identity.default_domain_id
+            return user
+        else:
+            return user
+
+    def list_users(self, hints):
+        sql_users = super(Identity, self).list_users(hints)
+        ldap_users = self.ldap.user.get_all_filtered(hints)
+        for user in ldap_users:
+            user['domain_id'] = CONF.identity.default_domain_id
+        return sql_users + ldap_users
+
+    def update_user(self, user_id, user):
+        session = sql.get_session()
+        user_ref = self._get_user(session, user_id)
+        # LDAP user_ref is a dict. SQL user_ref is a User object
+        if isinstance(user_ref, dict):
+            return self.ldap.update_user(user_id, user)
+        return super(Identity, self).update_user(user_id, user)
